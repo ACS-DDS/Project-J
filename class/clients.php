9e9bf5ae41ce7f29dfeae505c968a5c65bc62853
yoyohko <yoyohko@gmail.com>
2017-01-13 11:25:24 +0100

    Update clients.php

--- class/clients.php
+++ class/clients.php
@@ -1,14 +1,27 @@
 <?php
 
 class Client{
-	private $prenom;
+	
 	private $nom;
+	private $prenom;
+	private $id;
+	private $adr;
+	private $cp;
+	private $loc;
 	private $type;
 
 	public function __construct($array){
-		$this->prenom = $array[0];
 		$this->nom = $array[1];
+		$this->prenom = $array[0];
+		$this->id = $array[2];
+		$this->adr = $array[5];
+		$this->cp = $array[6];
+		$this->loc = $array[7];
 		$this->type = $array[4];
+	}
+
+	public function getType() {
+		return $this->type;
 	}
 }
 ?>
